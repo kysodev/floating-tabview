e883d8c2a6099b357772b2bc1439dc80b12041a9
kysodev <sebastianwhitfield@live.co.uk>
2022-11-11 16:27:05 +0000

    Initial commit

---
+++ floatingTabView/floatingTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  floatingTabViewApp.swift
+//  floatingTabView
+//
+//  Created by Sebastian Whitfield on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct floatingTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
